a4367fdd98fcb3c607caeaf8c324fc34677886b7
TatyanaErmakova99 <44719695+TatyanaErmakova99@users.noreply.github.com>
2019-04-06 11:42:17 +0600

    Update Generator.java

--- Generator.java
+++ Generator.java
@@ -2,13 +2,13 @@ import java.util.Arrays;
 
 public class Generator {
     public int[] gener(int a) {
-        //исходный массив
+        //РёСЃС…РѕРґРЅС‹Р№ РјР°СЃСЃРёРІ
         System.out.println();
         int[] mas = new int[a];
         for (int i = 0; i < mas.length; i++) {
             mas[i] = (int) Math.round(Math.random() * 100);
         }
-        System.out.println("Исходный массив: "+Arrays.toString(mas));
+        System.out.println("РСЃС…РѕРґРЅС‹Р№ РјР°СЃСЃРёРІ: "+Arrays.toString(mas));
         return mas;
     }
 }
