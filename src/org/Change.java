b35e9f51aa6a78a8ee46db2aa4622763daa2e00d
TatyanaErmakova99 <44719695+TatyanaErmakova99@users.noreply.github.com>
2019-05-31 15:48:01 +0700

    Add files via upload

---
+++ src/org/Change.java
@@ -0,0 +1,9 @@
+package org;
+
+public class Change {
+    public void swap(int []mas, int index){
+        int temp = mas[index-1];
+        mas[index-1]=mas[index];
+        mas[index]=temp;
+    }
+}
---
+++ src/org/Sheycker.java
@@ -0,0 +1,36 @@
+package org;
+
+import java.util.Arrays;
+
+public class Sheycker extends Generator {
+    public void SheyckerSort(int []mas){
+        long st, en, res;
+        st = System.nanoTime();
+        int left = 1, right = mas.length - 1;
+        do {
+
+            for (int i = right; i >= left; i--) {
+                if (mas[i - 1] > mas[i]) {
+                    int temp = mas[i];
+                    mas[i] = mas[i - 1];
+                    mas[i - 1] = temp;
+                    System.out.println(Arrays.toString(mas));
+                }
+            }
+            left++;
+            for (int i = left; i <= right; i++) {
+                if (mas[i - 1] > mas[i]) {
+                    int temp = mas[i];
+                    mas[i] = mas[i - 1];
+                    mas[i - 1] = temp;
+                    System.out.println(Arrays.toString(mas));
+                }
+            }
+            right--;
+
+        } while (left<=right);
+        en = System.nanoTime();
+        res = en - st;
+        System.out.println("time: "+res+" ns");
+    }
+}
